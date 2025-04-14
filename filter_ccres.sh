a9fd01712b60686c3c2f8473128b7fc859164b3f
prhamey <rhamey.p@northeastern.edu>
2025-04-14 18:22:50 -0400

    Update filter_ccres.sh
    Filter only for CTCF-bound elements

--- filter_ccres.sh
+++ filter_ccres.sh
@@ -4,17 +4,17 @@
 echo "Extracting cCRE IDs from allcCRE.txt..."
 tail -n +2 allcCRE.txt | cut -d ',' -f1 > ccre_ids.txt
 
-#  Filter for proximal elements that are CTCF-bound using V2.hg38-cCREs-Final.bed
-echo "Filtering for proximal (PLS or pELS) and CTFC-bound elements..."
-awk '$6 ~ /(CTCF-bound|CTCF-only)/ && $6 ~ /(PLS|pELS)/' V2.hg38-cCREs-Final.bed > ctcf_proximal.bed
+#  Filter for elements that are CTCF-bound using V2.hg38-cCREs-Final.bed
+echo "Filtering for CTFC-bound elements..."
+awk '$6 ~ /(CTCF-bound|CTCF-only)/' V2.hg38-cCREs-Final.bed > ctcf_elements.bed
 
 #  Match allcCRE.txt to filtered cCREs
 echo "Matching cCRE IDs..."
-awk -v OFS='\t' 'FNR==NR {ids[$1]; next} ($5 in ids)' ccre_ids.txt ctcf_proximal.bed > filtered_cCREs.bed
+awk -v OFS='\t' 'FNR==NR {ids[$1]; next} ($5 in ids)' ccre_ids.txt ctcf_elements.bed > filtered_cCREs.bed
 echo "Common elements saved to filtered_cCREs.bed"
 
 #  Cleanup intermediate files
-rm ccre_ids.txt ctcf_proximal.bed
+rm ccre_ids.txt ctcf_elements.bed
 
 #  Extract matched cCRE IDs to .txt
 cut -f5 filtered_cCREs.bed | sort -u > filtered_names.txt
@@ -23,4 +23,3 @@ echo "Matched cCRE IDs saved to filtered_names.txt"
 # Output number of matched cCREs
 num_matches=$(wc -l < filtered_names.txt)  # Counts lines in filtered_names.txt
 echo "Number of matched cCREs: $num_matches"
-
