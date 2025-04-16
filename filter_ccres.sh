8ff2961937e88ab84d53178f87f76e1a3da12da6
prhamey <rhamey.p@northeastern.edu>
2025-04-16 08:37:01 -0400

    Update filter_ccres.sh
    Add filtering by tissue type

--- filter_ccres.sh
+++ filter_ccres.sh
@@ -12,17 +12,27 @@ awk '$6 ~ /(CTCF-bound|CTCF-only)/ && $6 ~ /(pELS)/' V2.hg38-cCREs-Final.bed > c
 echo "Matching cCRE IDs..."
 awk -v OFS='\t' 'FNR==NR {ids[$1]; next} ($5 in ids)' ccre_ids.txt ctcfprox_elements.bed > prefiltered_cCREs.bed
 
-#  Format .bed file for additional processing/filtering (by tissue type)
+#  Format .bed file for additional processing/filtering
 cut -f1-3,5 prefiltered_cCREs.bed > filtered_cCREs.bed
 echo "Common elements saved to filtered_cCREs.bed"
 
-#  Cleanup intermediate files
-rm ccre_ids.txt ctcfprox_elements.bed
-
 #  Extract matched cCRE IDs to .txt
 cut -f4 filtered_cCREs.bed | sort -u > filtered_names.txt
-echo "Matched cCRE IDs saved to filtered_names.txt"
 
-# Output number of matched cCREs
+#  Output number of matched cCREs
 num_matches=$(wc -l < filtered_names.txt)  # Counts lines in filtered_names.txt
 echo "Number of matched cCREs: $num_matches"
+
+#  Find filtered cCREs that are also in filtered_cCREs_brain.bed
+awk 'FNR==NR {ids[$4]; next} $4 in ids' filtered_cCREs_brain.bed filtered_cCREs.bed > tissue_cCREs.bed
+echo "Common tissue-specific (brain) elements saved to tissue_cCREs.bed"
+
+#  Extract matched tissue cCRE IDs to .txt
+cut -f4 tissue_cCREs.bed | sort -u > filtered_tissue_names.txt
+
+#  Output number of matched tissue-specific cCREs
+num_tissue_matches=$(wc -l < filtered_tissue_names.txt)  # Counts lines in filtered_tissue_names.txt
+echo "Number of matched tissue-specific cCREs: $num_tissue_matches"
+
+#  Cleanup intermediate files
+rm ccre_ids.txt ctcfprox_elements.bed filtered_names.txt filtered_tissue_names.txt
