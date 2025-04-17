96cc5d0ba90637ce1b89ae807cde0236c0445ad7
prhamey <rhamey.p@northeastern.edu>
2025-04-17 19:41:09 -0400

    Update reconstruct_ssvs.sh

--- reconstruct_ssvs.sh
+++ reconstruct_ssvs.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 #  Stitch file back together
-cat chunk_* > reconstructed.bed
+cat results_* > reconstructed.bed
 
 #  Filter out SNPs
 awk '($3 - $2) > 1' reconstructed.bed > filtered_ssvs.bed
+echo "Filtered SSVs (no SNPs) saved to filtered_ssvs.bed"
+
+#  Remove intermediate files
+rm reconstructed.bed
